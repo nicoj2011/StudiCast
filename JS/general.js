b825d11d2ef9fa03f64cfc5c4def4f4dd9347371
Bromel <49148900+ehrlluca@users.noreply.github.com>
2019-11-14 10:51:59 +0100

    Vorlesung 14.11.2019

--- JS/general.js
+++ JS/general.js
@@ -1,35 +1,55 @@
+$(document).ready ( function(){
+    tabs("Podcast");
+    toggledLogin = false;
 
-/* tabs("Podcast"); */
-
-
+});
 
 function tabs(tab)
 {
     console.log("test");
     document.getElementById("tabPodcast").style.display = "none";
+    document.getElementById("hTabPodcast").style.color = "rgba(0,0,0,0.3)";
     document.getElementById("tabNews").style.display = "none";
+    document.getElementById("hTabNews").style.color = "rgba(0,0,0,0.3)";
     document.getElementById("tabArchiv").style.display = "none";
+    document.getElementById("hTabArchiv").style.color = "rgba(0,0,0,0.3)";
     document.getElementById("tabUmfrage").style.display = "none";
+    document.getElementById("hTabUmfrage").style.color = "rgba(0,0,0,0.3)";
     document.getElementById("tabKontakt").style.display = "none";
+    document.getElementById("hTabKontakt").style.color = "rgba(0,0,0,0.3)";
 
     if(tab == "Podcast")
         {
             document.getElementById("tabPodcast").style.display = "inherit";
+            document.getElementById("hTabPodcast").style.color = "rgba(0,0,0,1)";
         }
     else if(tab == "News")
         {
             document.getElementById("tabNews").style.display = "inherit";
+            document.getElementById("hTabNews").style.color = "rgba(0,0,0,1)";
         }
      else if(tab == "Archiv")
         {
             document.getElementById("tabArchiv").style.display = "inherit";
+            document.getElementById("hTabArchiv").style.color = "rgba(0,0,0,1)";
         }
      else if(tab == "Umfrage")
         {
             document.getElementById("tabUmfrage").style.display = "inherit";
+            document.getElementById("hTabUmfrage").style.color = "rgba(0,0,0,1)";
         }
      else if(tab == "Kontakt")
         {
             document.getElementById("tabKontakt").style.display = "inherit";
+            document.getElementById("hTabKontakt").style.color = "rgba(0,0,0,1)";
         }
 }
+
+$(function() {
+    $( "#profilBTN" ).click(function()
+    {
+
+        $( "#loginDiv" ).toggle( "blind" );
+
+    });
+});
